3c3572b34cfeccbe0e6dfcc09d1c54f100d15a7b
Luna <pamelalunafreire@gmail.com>
2025-11-14 02:03:02 +0000

    Add files via upload

---
+++ 08. Simple Search/README.md
@@ -0,0 +1,35 @@
+# Simple Search
+
+## Description
+This program performs a simple search inside a fixed array of strings.  
+The array contains **six** names:  
+"Jake", "Zac", "Ian", "Ron", "Sam", "Dave".  
+The program loops through the array and checks each position until it finds the name `Sam`.  
+If name is found, it prints the position in two ways:
+- **1based position** (human-friendly counting)
+- **0-based index** (programmer counting)
+
+## How to run
+- 1 Open the terminal in VS Code.
+- 2 Compile the program:  
+g++SimpleSearch.cpp -o SimpleSearch
+
+- 3 Execute
+- On Windows:  
+.\ SimpleSearch.exe
+- On MacOS / Linux:  
+./SimpleShearch
+
+## What I learned
+- How to create and use a `fixed array` in C++.
+- How to compare `strings` using the equality operator.
+- The difference btween `array index (0-based)` and `human position(1-based)`.
+- How to stop a loop early using `break` when a match is found.
+
+## Academic Integrity Note
+I use ChatGPT to study and understand the task requirements.
+After learning the concepts, I adapted and wrote the **final version** of the code myself.
+
+## Author
+**Pamela Luna**  
+Bath Spa University - Computing Class.
---
+++ 08. Simple Search/SimpleSearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream> // Provides cout and cin for input and output.
+#include <vector> // Provides the string data type.
+using namespace std;
+
+int main() {
+
+    // Create a fixed array of names with 6 strings.
+    string names[6] = {"Jake", "Zac", "Ian", "Ron", "Sam", "Dave"};
+    
+    string searchName = "Sam";  //The target name we are shearching for is "Sam".
+
+    // This variable will store the index where the name is found.
+    // Start with -1 to show that nothing was found yet.
+    int foundIndex = -1; 
+
+    // Loop through all elements of the array.
+    // i goes from 0 to 5 (because the array has 6 elements).
+    for (int i = 0; i < 6; i++) {
+
+        if (names[i] == searchName) { // Compare each element with the target "Sam".
+            foundIndex = i; // Store the index where it was found
+            break; // Stop the loop because we found the name.
+        }
+    }
+
+    if (foundIndex == -1) { //After the loop, check if the name is found.
+        cout << "The name '" << searchName << "' was not found.\n"; // -1 means no match was found. 
+    } else {
+        cout << "The name '" << searchName << "' was found!\n"; // If found, print both index styles.
+        cout << "Position in the list (1-based): " << foundIndex + 1 << "\n"; // Human-friendly position (starts at 1).
+        cout << "Array index (0-based): " << foundIndex + 1 << "\n"; // Programmer index (starts at 0).
+    }
+
+
+    return 0; // Program ends successfully.
+}
