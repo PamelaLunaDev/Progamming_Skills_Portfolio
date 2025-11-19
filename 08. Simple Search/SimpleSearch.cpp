98cde70eef756e20baaa333cb97552082e77e4b8
Luna <pamelalunafreire@gmail.com>
2025-11-19 14:34:36 +0000

    Add files via upload

--- 08. Simple Search/SimpleSearch.cpp
+++ 08. Simple Search/SimpleSearch.cpp
@@ -1,5 +1,4 @@
 #include <iostream> // Provides cout and cin for input and output.
-#include <vector> // Provides the string data type.
 using namespace std;
 
 int main() {
@@ -7,7 +6,7 @@ int main() {
     // Create a fixed array of names with 6 strings.
     string names[6] = {"Jake", "Zac", "Ian", "Ron", "Sam", "Dave"};
     
-    string searchName = "Sam";  //The target name we are shearching for is "Sam".
+    string searchName = "Sam";  // The target name we are searching for is "Sam".
 
     // This variable will store the index where the name is found.
     // Start with -1 to show that nothing was found yet.
@@ -23,12 +22,12 @@ int main() {
         }
     }
 
-    if (foundIndex == -1) { //After the loop, check if the name is found.
+    if (foundIndex == -1) { // After the loop, check if the name is found.
         cout << "The name '" << searchName << "' was not found.\n"; // -1 means no match was found. 
     } else {
         cout << "The name '" << searchName << "' was found!\n"; // If found, print both index styles.
-        cout << "Position in the list (1-based): " << foundIndex + 1 << "\n"; // Human-friendly position (starts at 1).
-        cout << "Array index (0-based): " << foundIndex + 1 << "\n"; // Programmer index (starts at 0).
+        cout << "Position in the list (1-based): " << foundIndex + 1  << "\n"; // Human-friendly position (starts at 1).
+        cout << "Array index (0-based): " << foundIndex << "\n"; // Programmer index (starts at 0).
     }
 
 
